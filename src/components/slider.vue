b4d9e4cf25df11297dd614caaed726375180d22c
yuchiu <yuchiu2002@hotmail.com>
2018-09-19 22:04:03 -0400

    setup slider template

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -1,7 +1,12 @@
 <template lang="html">
     <section :class="cname">
-        <swiper>
-            <swiper-slide>a</swiper-slide>
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+              <router-link :to="{name:item.href }">
+                <img :src="item.src" alt="">
+              </router-link>
+            </swiper-slide>
+            <div class="swiper-pagination" v-if="options.pagination">
         </swiper>
     </section>
 </template>
@@ -9,15 +14,34 @@
 <script>
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
   props: {
+    options: {
+      type: Object,
+      default() {
+        return {
+          autoplay: true,
+          loop: true,
+          pagination: {
+            el: ".swiper-pagination",
+            notNextTick: false
+          }
+        };
+      }
+    },
     cname: {
       type: String,
       default: ""
     }
   },
-  components: {
-    swiper,
-    swiperSlide
+  items: {
+    type: Array,
+    default() {
+      return [];
+    }
   }
 };
 </script>
